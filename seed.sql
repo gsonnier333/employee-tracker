0a6b1a3ffe454843f4ef9e2fee4c81213ca51ede
Greg Sonnier <gsonnier3@gmail.com>
2021-05-05 15:43:47 -0600

    improved seed file for usability, truncates instead of deleting from tables when run now

--- seed.sql
+++ seed.sql
@@ -1,14 +1,14 @@
 USE manage_db;
 
-DELETE FROM departments;
-DELETE FROM roles;
-DELETE FROM employees;
+TRUNCATE TABLE departments;
+TRUNCATE TABLE roles;
+TRUNCATE TABLE employees;
 
 INSERT INTO departments (name) VALUE ("Customer Service");
 INSERT INTO departments (name) VALUE ("Marketing");
 
-INSERT INTO roles (title, salary, department_id) VALUES ("Tech Support", 35000, 9);
-INSERT INTO roles (title, salary, department_id) VALUES ("Trends Analyst", 120000, 10);
+INSERT INTO roles (title, salary, department_id) VALUES ("Tech Support", 35000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ("Trends Analyst", 120000, 2);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Cherry", "Dude", 1, 2);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("Brad", "Vance", 1);
 INSERT INTO employees (first_name, last_name, role_id) VALUES ("Robert", "Peoples", 2);
